f061952c999168dda54577863eec8a8949a9a78b
tanuch <tanuch@users.noreply.github.com>
2017-07-31 19:30:13 +0700

    Update reload_i18n.sh

--- reload_i18n.sh
+++ reload_i18n.sh
@@ -3,9 +3,9 @@
 
 cd /tmp
 git clone https://github.com/tanuch/discourse-th.git
-cp -rT discourse-th/ /var/www/discourse/
+cp -rT discourse-th/ /var/discourse/
 cp -f discourse-th/reload_i18n.sh /root/reload_i18n.sh
 chmod +x /root/reload_i18n.sh
 rm -rf discourse-th
-cd /var/www/discourse
+cd /var/discourse
 sudo -u discourse RAILS_ENV=production bundle exec rake assets:clobber assets:precompile
